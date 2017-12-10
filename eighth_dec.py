7deb1efd00c398f4365e7906c126bdadbcdea122
Jakob Strutz <jakob@strutz.rocks>
2017-12-10 10:21:47 +0100

    Day 8 Part 1

---
+++ eighth_dec.py
@@ -0,0 +1,49 @@
+import sys
+import re
+
+TEST_STRING = """b inc 5 if a > 1
+a inc 1 if b < 5
+c dec -10 if a >= 1
+c inc -20 if c == 10"""
+
+MY_INPUT  = open("inputs/eighth.txt","r").read()
+regs = dict()
+
+def part1(input_string):
+    for line in input_string.splitlines():
+        groups = re.findall(r"(\w+) (inc|dec) (-?[\d]+) if (\w+) ([!=<>]+) (-?[\d]+)", line)[0]
+        print(groups)
+        if groups[3] not in regs:
+            regs[groups[3]] = 0
+        if condition(regs[groups[3]], groups[4], groups[5]) == True:
+            if groups[0] not in regs:
+                regs[groups[0]] = 0
+            regs[groups[0]] = instruction(groups[1], regs[groups[0]], groups[2])
+        print(groups)
+
+    return 1
+
+def instruction(inst, var, val):
+    if inst == "dec":
+        var -= int(val)
+    elif inst == "inc":
+        var += int(val)
+    return var
+
+def condition(var, op, val):
+    if op == "<":
+        return int(var) < int(val)
+    elif op == "<=":
+        return int(var) <= int(val)
+    elif op == "==":
+        return int(var) == int(val)
+    elif op == ">=":
+        return int(var) >= int(val)
+    elif op == ">":
+        return int(var) > int(val)
+    elif op == "!=":
+        return int(var) != int(val)
+
+part1(MY_INPUT)
+print(regs[max(regs, key=lambda i: regs[i])])
+sys.exit(0)
